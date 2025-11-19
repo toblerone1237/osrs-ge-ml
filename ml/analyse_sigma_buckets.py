d3aca9f3685581fe3e6e58a12adaba2bd2e9434d
toblerone1237 <toblerone1237@gmail.com>
2025-11-19 06:39:42 +0000

    Update analyse_sigma_buckets.py

--- ml/analyse_sigma_buckets.py
+++ ml/analyse_sigma_buckets.py
@@ -98,8 +98,8 @@ def build_eval_dataframe(s3, bucket: str) -> pd.DataFrame:
 
 
 def load_latest_regressor(s3, bucket):
-    key_reg = "models/xgb/latest_reg.pkl"
-    key_meta = "models/xgb/latest_meta.json"
+    key_reg = "models/remove-noisy-sections/xgb/latest_reg.pkl"
+    key_meta = "models/remove-noisy-sections/xgb/latest_meta.json"
 
     obj_reg = s3.get_object(Bucket=bucket, Key=key_reg)
     obj_meta = s3.get_object(Bucket=bucket, Key=key_meta)
