483dafa1a4601a1faa94c31d0c517ee6c1327615
toblerone1237 <toblerone1237@gmail.com>
2025-12-13 01:00:58 +0000

    Changes to be committed: 	modified:   workers/osrs-ge-ui/src/index.js

--- workers/osrs-ge-ui/src/index.js
+++ workers/osrs-ge-ui/src/index.js
@@ -32,7 +32,7 @@ const HTML = `<!DOCTYPE html>
       color: #9ca3af;
     }
     main {
-      max-width: 1550px;
+      max-width: 100%;
       margin: 1.5rem auto 3rem;
       padding: 0 1rem;
     }
